de46f704bd1cd00cc5f84496bc001c109f722098
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-28 10:02:31 -0400

    Configure mongoose

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
